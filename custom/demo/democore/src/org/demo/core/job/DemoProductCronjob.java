946a9c84b04580768741cd1cdf97f91a8444c530
Apnan Juanda <apnanjuanda@gmail.com>
2022-02-15 07:59:14 +0700

    Cronjob Approval Product

---
+++ custom/demo/democore/src/org/demo/core/job/DemoProductCronjob.java
@@ -0,0 +1,53 @@
+package org.demo.core.job;
+
+import de.hybris.platform.catalog.enums.ArticleApprovalStatus;
+import de.hybris.platform.cronjob.enums.CronJobResult;
+import de.hybris.platform.cronjob.enums.CronJobStatus;
+import de.hybris.platform.cronjob.model.CronJobModel;
+import de.hybris.platform.servicelayer.cronjob.AbstractJobPerformable;
+import de.hybris.platform.servicelayer.cronjob.PerformResult;
+import de.hybris.platform.servicelayer.model.ModelService;
+import org.demo.core.model.DemoProductModel;
+import org.demo.core.model.DemoVariantProductModel;
+import org.demo.core.product.dao.DemoProductDAO;
+
+import javax.annotation.Resource;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class DemoProductCronjob extends AbstractJobPerformable<CronJobModel> {
+    private static final Logger LOG = Logger.getLogger(DemoProductCronjob.class);
+    @Resource
+    ModelService modelService;
+
+    @Resource
+    DemoProductDAO demoProductDAO;
+
+
+    @Override
+    public PerformResult perform(CronJobModel cronJobModel) {
+        LOG.info("Demo Product Cronjob was trigger");
+
+        List<Object> products = demoProductDAO.getAllProduct();
+        if(products != null){
+            for(Object  product: products){
+                if(product.getClass() == DemoProductModel.class){
+                    if(((DemoProductModel) product).getApprovalStatus() == ArticleApprovalStatus.CHECK){
+                        ((DemoProductModel) product).setApprovalStatus(ArticleApprovalStatus.APPROVED);
+                        modelService.save(product);
+                    }
+                }else{
+                    if(((DemoVariantProductModel) product).getApprovalStatus() == ArticleApprovalStatus.CHECK){
+                        ((DemoVariantProductModel) product).setApprovalStatus(ArticleApprovalStatus.APPROVED);
+                        modelService.save(product);
+                    }
+                }
+            }
+        }else{
+            LOG.info("Data Product is empty");
+        }
+
+        return new PerformResult(CronJobResult.SUCCESS, CronJobStatus.FINISHED);
+    }
+}
---
+++ custom/demo/democore/src/org/demo/core/product/dao/DemoProductDAO.java
@@ -0,0 +1,9 @@
+package org.demo.core.product.dao;
+
+import org.demo.core.model.DemoProductModel;
+
+import java.util.List;
+
+public interface DemoProductDAO {
+    List<Object> getAllProduct();
+}
