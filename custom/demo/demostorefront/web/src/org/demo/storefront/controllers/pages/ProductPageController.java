1a4be3794ac7cdbf52f75ae160af1d77ab18d93b
Haidar Haryokusumo <haidaraide@gmail.com>
2022-02-21 16:48:56 +0700

    revert

--- custom/demo/demostorefront/web/src/org/demo/storefront/controllers/pages/ProductPageController.java
+++ custom/demo/demostorefront/web/src/org/demo/storefront/controllers/pages/ProductPageController.java
@@ -3,8 +3,6 @@
  */
 package org.demo.storefront.controllers.pages;
 
-import de.hybris.platform.acceleratorcms.jalo.components.CartSuggestionComponent;
-import de.hybris.platform.acceleratorcms.model.components.CartSuggestionComponentModel;
 import de.hybris.platform.acceleratorfacades.futurestock.FutureStockFacade;
 import de.hybris.platform.acceleratorservices.controllers.page.PageType;
 import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.impl.ProductBreadcrumbBuilder;
@@ -17,7 +15,6 @@ import de.hybris.platform.acceleratorstorefrontcommons.forms.validation.ReviewVa
 import de.hybris.platform.acceleratorstorefrontcommons.util.MetaSanitizerUtil;
 import de.hybris.platform.acceleratorstorefrontcommons.util.XSSFilterUtil;
 import de.hybris.platform.acceleratorstorefrontcommons.variants.VariantSortStrategy;
-import de.hybris.platform.catalog.enums.ProductReferenceTypeEnum;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
 import de.hybris.platform.cms2.model.pages.AbstractPageModel;
 import de.hybris.platform.cms2.servicelayer.services.CMSPageService;
@@ -35,12 +32,16 @@ import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.product.ProductService;
 import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
 import de.hybris.platform.util.Config;
-import org.demo.facades.productSuggestion.DemoSuggestionFacade;
-import org.demo.facades.suggestion.SimpleSuggestionFacade;
 import org.demo.storefront.controllers.ControllerConstants;
 
 import java.io.UnsupportedEncodingException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -110,12 +111,6 @@ public class ProductPageController extends AbstractPageController
 	@Resource(name = "futureStockFacade")
 	private FutureStockFacade futureStockFacade;
 
-	@Resource(name = "simpleDemoSuggestionFacade")
-	private DemoSuggestionFacade demoSuggestionFacade;
-
-	@Resource(name = "simpleSuggestionFacade")
-	private SimpleSuggestionFacade simpleSuggestionFacade;
-
 	@RequestMapping(value = PRODUCT_CODE_PATH_VARIABLE_PATTERN, method = RequestMethod.GET)
 	public String productDetail(@PathVariable("productCode") final String productCode, final Model model,
 			final HttpServletRequest request, final HttpServletResponse response)
@@ -138,16 +133,8 @@ public class ProductPageController extends AbstractPageController
 		populateProductDetailForDisplay(productCode, model, request, extraOptions);
 
 		model.addAttribute(new ReviewForm());
-		model.addAttribute("pageType", PageType.PRODUCT.name()); //ini valuenya: PRODUCT
-		model.addAttribute("futureStockEnabled", Boolean.valueOf(Config.getBoolean(FUTURE_STOCK_ENABLED, false))); //ini nilainya false
-
-		//------------Check Demo Suggestion Facade Impl--------------------//
-		Set<String> code = new HashSet<String>();
-		code.add(productCode);
-		List<ProductReferenceTypeEnum> referenceType = new ArrayList<ProductReferenceTypeEnum>();
-		referenceType.add(ProductReferenceTypeEnum.BASE_PRODUCT);
-		model.addAttribute("suggestions", demoSuggestionFacade.getReferencesForProducts(code,referenceType, true, 5));
-		//model.addAttribute("suggestion", simpleSuggestionFacade.getSuggestionsForProductsInCart(, component.isFilterPurchased(), component.getMaximumNumberProducts()));
+		model.addAttribute("pageType", PageType.PRODUCT.name());
+		model.addAttribute("futureStockEnabled", Boolean.valueOf(Config.getBoolean(FUTURE_STOCK_ENABLED, false)));
 
 		final String metaKeywords = MetaSanitizerUtil.sanitizeKeywords(productData.getKeywords());
 		final String metaDescription = MetaSanitizerUtil.sanitizeDescription(productData.getDescription());
@@ -419,7 +406,7 @@ public class ProductPageController extends AbstractPageController
 
 		sortVariantOptionData(productData);
 		storeCmsPageInModel(model, getPageForProduct(productCode));
-		populateProductData(productData, model); //Memasukkan data dengan attribute product.
+		populateProductData(productData, model);
 		model.addAttribute(WebConstants.BREADCRUMBS_KEY, productBreadcrumbBuilder.getBreadcrumbs(productCode));
 
 		if (CollectionUtils.isNotEmpty(productData.getVariantMatrix()))
@@ -431,11 +418,8 @@ public class ProductPageController extends AbstractPageController
 
 	protected void populateProductData(final ProductData productData, final Model model)
 	{
-		model.addAttribute("galleryImages", getGalleryImages(productData)); //Untuk memasukkan gambar di page PDP
+		model.addAttribute("galleryImages", getGalleryImages(productData));
 		model.addAttribute("product", productData);
-
-
-
 		if (productData.getConfigurable())
 		{
 			final List<ConfigurationInfoData> configurations = productFacade.getConfiguratorSettingsForCode(productData.getCode());
--- custom/demo/demostorefront/web/webroot/WEB-INF/views/responsive/pages/product/productLayout2Page.jsp
+++ custom/demo/demostorefront/web/webroot/WEB-INF/views/responsive/pages/product/productLayout2Page.jsp
@@ -3,21 +3,9 @@
 <%@ taglib prefix="cms" uri="http://hybris.com/tld/cmstags"%>
 <%@ taglib prefix="product" tagdir="/WEB-INF/tags/responsive/product"%>
 
-<%----------------FROM SIMPLE SUGGESTION COMPONENT----------------%>
-<%@ page trimDirectiveWhitespaces="true" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-<%@ taglib prefix="format" tagdir="/WEB-INF/tags/shared/format" %>
-<%@ taglib prefix="component" tagdir="/WEB-INF/tags/shared/component" %>
-<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
-
-<spring:htmlEscape defaultHtmlEscape="true" />
-
 
 <template:demoPage pageTitle="${pageTitle}">
-    <%-----MAIN PRODUCT DETAIL-----%>
 	<product:productDetailsPanel />
-
 	<div class="row item">
             <cms:pageSlot position="Keterangan" var="feature" element="div" class="keterangan">
                  <cms:component component="${feature}" element="div" class=""/>
@@ -26,47 +14,5 @@
 	<cms:pageSlot position="CrossSelling" var="comp" element="div" class="productDetailsPageSectionCrossSelling">
 		<cms:component component="${comp}" element="div" class="productDetailsPageSectionCrossSelling-component"/>
 	</cms:pageSlot>
-
-    <%-----SUGGESTION PRODUCTS-----%>
-    <div id="quickViewTitle" class="quickView-header display-none"><spring:theme code="popup.quick.view.select"/></div>
-    
-    <c:choose>
-        <%-----REQUIREMENT MAXIMUM PRODUCTS: FIVE-----%>
-
-    	<c:when test="${not empty suggestions and component.maximumNumberProducts > 0}">
-    		<div class="carousel-component">
-    			<div class="headline">${fn:escapeXml(component.title)}</div>
-    			<div class="carousel js-owl-carousel js-owl-lazy-reference js-owl-carousel-reference">
-
-    			    <%-----SHOWING PRODUCTS SUGGESTION-----%>
-    				<c:forEach end="${component.maximumNumberProducts}" items="${suggestions}" var="suggestion">
-    					<c:url value="${suggestion.url}/quickView" var="productQuickViewUrl"/>
-    					<div class="item">
-    						<a href="${fn:escapeXml(productQuickViewUrl)}" class="js-reference-item">
-
-                                 <%-----SHOWING IMAGE SUGGESTION-----%>
-                                <div class="thumb">
-                                    <product:productPrimaryReferenceImage product="${suggestion}" format="product"/>
-                                </div>
-
-                                 <%-----SHOWING TITLE SUGGESTION-----%>
-    							<c:if test="${component.displayProductTitles}">
-    								<div class="item__name">${fn:escapeXml(suggestion.name)}</div>
-    							</c:if>
-
-                                <%-----SHOWING PRICE SUGGESTION-----%>
-    							<c:if test="${component.displayProductPrices}">
-    								<div class="item__price"><format:fromPrice priceData="${suggestion.price}"/></div>
-    							</c:if>
-    						</a>
-    					</div>
-    				</c:forEach>
-    			</div>
-    		</div>
-    	</c:when>
-    	<c:otherwise>
-    		<component:emptyComponent/>
-    	</c:otherwise>
-    </c:choose>
 
 </template:demoPage>
